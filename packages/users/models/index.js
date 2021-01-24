deac347896637b62dbe31f3866c0d6770bbf5786
Rexben <rexben.rb@gmail.com>
2021-01-24 19:11:01 +0100

    Update index.js

--- packages/users/models/index.js
+++ packages/users/models/index.js
@@ -4,7 +4,6 @@ const Users = require('./user');
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 
-console.log('process.env.MONGODB_URI>>>', process.env.MONGODB_URI);
 const connectDB = () =>
   mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
